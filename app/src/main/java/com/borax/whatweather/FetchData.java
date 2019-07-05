f06fcd837b5e664bd328e71730292f872c3d0f56
Supratim Chakraborty <sup270792@gmail.com>
2019-07-05 14:56:08 -0700

    Refactored to work on Android Studio and Change ClearText HTTP exception

---
+++ app/src/main/java/com/borax/whatweather/FetchData.java
@@ -0,0 +1,51 @@
+package com.borax.whatweather;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+
+public class FetchData {
+	
+	private static final String OPEN_WEATHER_MAP_API = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+	
+	public static JSONObject getJSON(Context context, String city)
+	{
+		try
+		{
+			URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			
+			connection.addRequestProperty("x-api-key", context.getString(R.string.openweather_map_id));
+			
+			BufferedReader bufferedReader=  new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+			StringBuffer json= new StringBuffer(1024);
+			String tmp="";
+			while((tmp=bufferedReader.readLine())!=null)
+			{
+				json.append(tmp).append("\n");
+				
+			}
+			bufferedReader.close();
+			
+			JSONObject data= new JSONObject(json.toString());
+			
+			if(data.getInt("cod") != 200)
+			{
+				return null;
+			}
+			
+			return data;
+		}
+		catch(Exception e)
+		{
+			return null;
+		}
+	}
+
+}
